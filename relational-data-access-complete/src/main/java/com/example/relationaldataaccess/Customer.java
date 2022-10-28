23f6725e9dab2c7dc4d3eedf06ab5cd018a9fa77
DAM2Alu10 <210176@iessierraguara.es>
2022-10-28 09:22:39 +0200

    proyecto jdbc

--- relational-data-access-complete/src/main/java/com/example/relationaldataaccess/Customer.java
+++ relational-data-access-complete/src/main/java/com/example/relationaldataaccess/Customer.java
@@ -4,6 +4,9 @@ public class Customer {
 	private long id;
 	private String firstName, lastName;
 
+	public Customer() {
+		
+	}
 	public Customer(long id, String firstName, String lastName) {
 		this.id = id;
 		this.firstName = firstName;
@@ -16,6 +19,26 @@ public class Customer {
 				"Customer[id=%d, firstName='%s', lastName='%s']",
 				id, firstName, lastName);
 	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
 	// getters & setters omitted for brevity
+	
+	
 }
--- relational-data-access-complete/src/main/java/com/example/relationaldataaccess/RelationalDataAccessApplication.java
+++ relational-data-access-complete/src/main/java/com/example/relationaldataaccess/RelationalDataAccessApplication.java
@@ -1,5 +1,11 @@
 package com.example.relationaldataaccess;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +13,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.jdbc.core.RowMapper;
 
 @SpringBootApplication
 public class RelationalDataAccessApplication implements CommandLineRunner {
@@ -30,13 +33,11 @@ public class RelationalDataAccessApplication implements CommandLineRunner {
 		log.info("Creating tables");
 
 		jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
-		jdbcTemplate.execute("CREATE TABLE customers(" +
-				"id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+		jdbcTemplate.execute("CREATE TABLE customers(" + "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
 
 		// Split up the array of whole names into an array of first/last names
 		List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
-				.map(name -> name.split(" "))
-				.collect(Collectors.toList());
+				.map(name -> name.split(" ")).collect(Collectors.toList());
 
 		// Use a Java 8 stream to print out each tuple of the list
 		splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
@@ -45,9 +46,32 @@ public class RelationalDataAccessApplication implements CommandLineRunner {
 		jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
 
 		log.info("Querying for customer records where first_name = 'Josh':");
-		jdbcTemplate.query(
+		jdbcTemplate.query("SELECT id, first_name, last_name FROM customers WHERE first_name = ?",//Primer argumento => sentencia SQL
+						   new Object[] { "Josh" },												  //Segundo argumento => ?
+						   (rs, rowNum) -> new Customer(rs.getLong("id"), 						  //Tercer argumento => Creamos el objeto 
+							       					    rs.getString("first_name"), 
+														rs.getString("last_name")
+														)
+						  ).forEach(customer -> log.info(customer.toString()));// Devuelve una lista y la recorrmos para imprimir.
+
+		// Añadido Alberto Carrera Martín Octubre 2022
+		// Ahora todos como se hacía sin stream ni lambdas
+		// Añadir en clase Customer.java los métodos getters y setters
+		// y también constructor vacío
+		//
+		log.info("Ahora sin utilizar stream ni lambdas");
+		List<Customer> lista = jdbcTemplate.query(
 				"SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
-				(rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
-		).forEach(customer -> log.info(customer.toString()));
+				new RowMapper<Customer>() {
+					public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+						Customer customer = new Customer();
+						customer.setId(rs.getLong("id"));
+						customer.setFirstName(rs.getString("first_name"));
+						customer.setLastName(rs.getString("last_name"));
+						return customer;
+					}
+
+				});
+		log.info(lista.toString());
 	}
 }

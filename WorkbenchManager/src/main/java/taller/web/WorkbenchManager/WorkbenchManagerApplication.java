fbd3ab594782446cd311739ef6cdff6b2c0228d0
DAM2Alu10 <210176@iessierraguara.es>
2023-02-22 12:37:27 +0100

    subo proyectos spring

---
+++ WorkbenchManager/src/main/java/taller/web/WorkbenchManager/WorkbenchManagerApplication.java
@@ -0,0 +1,13 @@
+package taller.web.WorkbenchManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbenchManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkbenchManagerApplication.class, args);
+	}
+
+}
---
+++ spring-boot-data-jpa1/src/main/resources/import.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1, 'Alberto', 'Carrera', 'alberto@gmail.com', '1998-07-13');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2, 'Luis', 'Aldea', 'luis@gmail.com', '2012-05-23');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (3, 'Belén', 'Carrera', 'belen@gmail.com', '2009-06-03'); 
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (4, 'Toño', 'Mériz', 'tono@gmail.com', '1999-03-12');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (5, 'Fernando', 'Martínez', 'fernando@gmail.com', '2012-04-07');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (6, 'Nacho', 'Garcés', 'nacho@gmail.com', '1998-06-17');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (7, 'Javier', 'Escartín', 'txavi@gmail.com', '2000-11-12');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (8, 'Lorenzo', 'Blasco', 'lorenzo@gmail.com', '2019-04-18');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (9, 'Fernando', 'Carrera', 'fernando2@gmail.com', '2019-04-18');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (10, 'Mariano', 'Carrera', 'mariano@gmail.com', '2019-04-18');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (11, 'Mario', 'Carrera', 'mario@gmail.com', '2019-04-18');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (12, 'Raquel', 'Carrera', 'raquel@gmail.com', '2019-04-18');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (13, 'Carmen', 'Bailín', 'carmen@gmail.com', '2019-04-18');

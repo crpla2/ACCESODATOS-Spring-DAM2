090dd2c9d57bf8e28d2dfdb7f25ea4dd9a6f0b54
crpla2 <210176@iessierradeguara.com>
2023-02-24 02:06:08 +0100

    ya

--- WorkbenchManager/src/main/java/taller/web/WorkbenchManager/model/dao/DatosDao.java
+++ WorkbenchManager/src/main/java/taller/web/WorkbenchManager/model/dao/DatosDao.java
@@ -2,6 +2,7 @@ package taller.web.WorkbenchManager.model.dao;
 
 import org.springframework.transaction.annotation.Transactional;
 
+import com.openwebinars.spring.modelo.Empleado;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -13,7 +14,19 @@ public class DatosDao {
 	private EntityManager em;
 
 	@Transactional(readOnly = true)
-	public Datos findOne(Long id) {
-		return em.find(Datos.class, id);
+	public Datos findById(long id) {
+		Datos result = null;
+		boolean encontrado = false;
+		int i = 0;
+		while (!encontrado && i < repositorio.size()) {
+			if (repositorio.get(i).getId() == id) {
+				encontrado = true;
+				result = repositorio.get(i);
+			} else {
+				i++;
+			}
+		}
+		
+		return result;
 	}
 }
